53e80e253b93832774a28740b8b51bb7b5b59114
Khalikov Vadim <75984650+sadside@users.noreply.github.com>
2024-04-14 13:02:04 +0700

    fix tests for lab6-0
    Fixed tests that were solved by incorrect tree rotations. added checking the sequence of turns when balancing

--- testlab6-0.c
+++ testlab6-0.c
@@ -4,47 +4,48 @@
 #include <string.h>
 
 static int testN = 0;
-static const struct {const char *const in; int n;} testInOut[] = {
-    {"0\n\n", 0},
-    {"1\n25\n", 1},
-    {"2\n17 25\n", 2},
-    {"3\n2 -1 4\n", 2},
-    {"3\n2 4 -1\n", 2},
-    {"3\n4 2 -1\n", 2},
-    {"3\n4 -1 2\n", 2},
-    {"3\n-1 4 2\n", 2},
-    {"3\n-1 2 4\n", 2},
-    {"3\n-1 -1 2\n", 2},
-    {"3\n-1 2 -1\n", 2},
-    {"3\n2 -1 -1\n", 2},
-    {"3\n-1 -1 -1\n", 2},
-    {"2\n-1 -1\n", 2},
-    {"2\n4 2\n", 2},
-    {"2\n2 4\n", 2},
-    {"4\n1 2 3 4\n", 3},
-    {"4\n1 2 4 3\n", 3},
-    {"4\n1 3 2 4\n", 3},
-    {"4\n1 3 4 2\n", 3},
-    {"4\n1 4 2 3\n", 3},
-    {"4\n1 4 3 2\n", 3},
-    {"4\n2 1 3 4\n", 3},
-    {"4\n2 1 4 3\n", 3},
-    {"4\n2 3 1 4\n", 3},
-    {"4\n2 3 4 1\n", 3},
-    {"4\n2 4 1 3\n", 3},
-    {"4\n2 4 3 1\n", 3},
-    {"4\n3 1 2 4\n", 3},
-    {"4\n3 1 4 2\n", 3},
-    {"4\n3 2 1 4\n", 3},
-    {"4\n3 2 4 1\n", 3},
-    {"4\n3 4 1 2\n", 3},
-    {"4\n3 4 2 1\n", 3},
-    {"4\n4 1 2 3\n", 3},
-    {"4\n4 1 3 2\n", 3},
-    {"4\n4 2 1 3\n", 3},
-    {"4\n4 2 3 1\n", 3},
-    {"4\n4 3 1 2\n", 3},
-    {"4\n4 3 2 1\n", 3},
+static const struct {const char *const in; const char *const out;} testInOut[] = {
+    // Possible errors
+    {"", "Empty input"},
+    {" ", "Empty input"},
+    {"0\n\n", "0"},
+    {"1\n0", "1"},
+    {"1\n1", "1"},
+    {"-1\n3", "Bad input"},
+    {"2000002\n2 5", "Bad input"},
+    {"2000000\n2 5", "Bad input"},
+        
+    // Small rotates variations
+    {"5\n3 5 8 9 10", "SL SL 3"},
+    {"5\n15 9 8 5 2", "SR SR 3"},
+    {"5\n3 5 8 1 0", "SL SR 3"},
+    {"5\n15 9 2 32 168", "SR SL 3"},
+        
+    // Pairs of small + big rotates variations
+    {"5\n1 2 3 5 4", "SL BL 3"},
+    {"5\n1 2 5 3 4", "SL BR 3"},
+    {"5\n3 2 1 5 4", "SR BL 3"},
+    {"5\n5 2 1 3 4", "SR BR 3"},
+        
+    // 2 small rotates and 2 big rotates in different order (start from small)
+    {"10\n12 32 168 9 2 200 170 1 3 4", "SL SR BL BR 4"},
+    {"8\n12 32 168 9 2 22 30 23", "SL SR BR BL 4"},
+    {"9\n12 32 168 64 200 48 11 10 22", "SL BL SR BR 4"},
+    {"10\n12 32 168 64 200 48 11 13 22 10", "SL BL BR SR 4"},
+    {"9\n12 32 168 22 10 15 9 8 11", "SL BR SR BR 4"},
+    {"10\n13 32 168 22 10 15 9 11 12 8", "SL BR BR SR 4"},
+    {"10\n12 9 2 32 168 22 10 3 1 4", "SR SL BL BR 4"},
+    {"11\n12 9 2 32 168 1 22 10 23 15 17", "SR SL BR BL 4"},
+        
+    // 2 small rotates and 2 big rotates in different order (start from big)
+    {"8\n25 52 39 13 9 35 64 259", "BL SR BR SL 4"},
+    {"9\n25 52 39 -5 38 30 67 -39 -18", "BL BR SL BR 4"},
+    {"8\n18 39 20 -25 10 -30 105 110", "BL BR SR SL 4"},
+    {"9\n52 25 39 87 99 14 0 112 105", "BR SL SR BL 4"},
+    {"7\n52 25 39 195 207 75 -8", "BR SL BL SR 3"},
+    {"9\n52 25 39 5 0 159 201 64 55", "BR SR SL BL 4"},
+    {"9\n17 10 13 4 -8 28 20 45 52", "BR SR BL SL 4"},
+    {"9\n5 20 10 30 40 0 3 -2 -5", "BL SL BR SR 4"},
 };
 
 static int FeedFromArray(void)
@@ -66,30 +67,37 @@ static int FeedFromArray(void)
 static int CheckFromArray(void)
 {
     FILE *const out = fopen("out.txt", "r");
-    int passed = 1;
     if (!out) {
         printf("can't open out.txt\n");
         testN++;
         return -1;
     }
-    {
-        int n;
-        if (ScanInt(out, &n) != Pass) {
-            passed = 0;
-        } else if (testInOut[testN].n != n) {
-            passed = 0;
-            printf("wrong output -- ");
-        }
+    
+    int passed;
+    char buffMsg[64] = {0};
+    
+    if (ScanChars(out, sizeof(buffMsg), buffMsg) != Pass) {
+        passed = 0;
     }
+    
+    else if (strcmp(testInOut[testN].out, buffMsg) != 0) {
+        passed = 0;
+        printf("wrong output --");
+    }
+    
+    
     if (passed) {
         passed = !HaveGarbageAtTheEnd(out);
     }
+    
     fclose(out);
     if (passed) {
         printf("PASSED\n");
         testN++;
         return 0;
-    } else {
+    } 
+    
+    else {
         printf("FAILED\n");
         testN++;
         return 1;
@@ -208,6 +216,7 @@ static int checkerBig1(void)
     }
 }
 
+
 static void genTree(FILE *in, int height, int min, int max)
 {
     int mid = (min*21+max*34)/55;
@@ -298,14 +307,6 @@ static int checkerBig2(void)
 }
 
 const TLabTest LabTests[] = {
-    {FeedFromArray, CheckFromArray},
-    {FeedFromArray, CheckFromArray},
-    {FeedFromArray, CheckFromArray},
-    {FeedFromArray, CheckFromArray},
-    {FeedFromArray, CheckFromArray},
-    {FeedFromArray, CheckFromArray},
-    {FeedFromArray, CheckFromArray},
-    {FeedFromArray, CheckFromArray},
     {FeedFromArray, CheckFromArray},
     {FeedFromArray, CheckFromArray},
     {FeedFromArray, CheckFromArray},
