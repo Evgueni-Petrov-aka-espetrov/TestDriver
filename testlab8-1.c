767dcb9c2e10706602bb3282230c7599ce4693e7
unknown <a.taskaev@g.nsu.ru>
2024-06-03 15:21:17 +0300

    Update testlab8

---
+++ testlab8-1.c
@@ -0,0 +1,88 @@
+#include "testlab8-base.h"
+#include "testLab.h"
+#include <stdlib.h>
+
+
+enum {
+    VERTEX_FOR_PRIM = 7000
+};
+
+
+
+
+
+
+int SpecialFeed()
+{
+    FILE* const in = fopen("in.txt", "w+");
+    if (in == NULL) {
+        printf("can't create in.txt. No space on disk?\n");
+        return -1;
+    }
+
+    const unsigned vertexCount = VERTEX_FOR_PRIM;
+    const unsigned edgeCount = SumRange(1, VERTEX_FOR_PRIM - 1);
+    fprintf(in, "%u\n%u\n", vertexCount, edgeCount);
+    const int isVerbose = edgeCount > 1000 * 1000;
+    if (isVerbose) {
+        printf("Creating large text... ");
+        fflush(stdout);
+    }
+
+    unsigned start = GetTickCount();
+    
+    for (unsigned begin = 1, lenght = 1; begin < vertexCount; begin++)
+    {
+        for (unsigned end = begin+1; end < vertexCount+1;end++,lenght++)
+        {
+            fprintf(in, "%u %u %u\n", begin, end, lenght);
+        }
+    }
+
+    fclose(in);
+    start = RoundUptoThousand(GetTickCount() - start);
+
+    if (isVerbose) {
+        printf("done in T=%u seconds. Starting exe with timeout T+3 seconds... ", start / 1000);
+        fflush(stdout);
+    }
+
+    LabTimeout = (int)start + 3000;
+    LabMemoryLimit = vertexCount * vertexCount * 4 + MIN_PROCESS_RSS_BYTES;
+
+    return 0;
+}
+
+
+
+TTestcaseData Lab8SpecialTest(enum ETestcaseDataId dataId, unsigned edgeIdx) {
+    unsigned partOneEdge = VERTEX_FOR_PRIM - 1;
+    unsigned partTwoEdge = SumRange(1, VERTEX_FOR_PRIM - 2);
+
+    switch (dataId) {
+    case VERTEX_COUNT:
+        return MakeInteger(VERTEX_FOR_PRIM);
+    case EDGE_COUNT:
+        return MakeInteger(partOneEdge+partTwoEdge);
+    case EDGE:
+        if (edgeIdx < partOneEdge)
+        {
+            return MakeEdge(1, edgeIdx + 2, edgeIdx + 1);
+        }
+        else {
+            unsigned row, col;
+            CalcRowColumn(edgeIdx-partOneEdge, &row, &col);
+            row = VERTEX_FOR_PRIM - row + col;
+            col = col + 2;
+
+            return MakeEdge(row, col, edgeIdx + 1);
+        }
+    case ERROR_MESSAGE:
+        return MakeString(NULL);
+    case MST_LENGTH:
+        return MakeInteger(SumRange(1, VERTEX_FOR_PRIM - 1));
+    default:
+        abort();
+    }
+
+}
