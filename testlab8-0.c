dce9f9fc5ab0a072880129a941a33c48ad8c6725
pshenichka123 <167999946+pshenichka123@users.noreply.github.com>
2024-06-03 18:38:19 +0700

    Update testlab8-0.c

--- testlab8-0.c
+++ testlab8-0.c
@@ -1,6 +1,6 @@
 #include "testlab8-base.h"
 #include "testLab.h"
-
+#include <stdlib.h>
 enum {
     VERTEX_FOR_KRUSKAL = 20000
 };
@@ -16,9 +16,7 @@ int SpecialFeed()
     const unsigned edgeCount = VERTEX_FOR_KRUSKAL - 1;
     fprintf(in, "%u\n%u\n", vertexCount, edgeCount);
     
-
     unsigned start = GetTickCount();
-
     for (unsigned begin = 1, lenght = 1; begin < vertexCount; begin++)
     {
         fprintf(in, "%u %u %u\n", begin, begin + 1, begin);
@@ -27,18 +25,13 @@ int SpecialFeed()
     fclose(in);
     start = RoundUptoThousand(GetTickCount() - start);
 
-
     LabTimeout = (int)start + 3000;
     LabMemoryLimit = 24 * (vertexCount + 1) * (vertexCount + 1) + 24 * vertexCount + MIN_PROCESS_RSS_BYTES;
 
     return 0;
 }
 
-
-
 TTestcaseData Lab8SpecialTest(enum ETestcaseDataId dataId, unsigned edgeIdx) {
-   
-
     switch (dataId) {
     case VERTEX_COUNT:
         return MakeInteger(VERTEX_FOR_KRUSKAL);
@@ -53,5 +46,4 @@ TTestcaseData Lab8SpecialTest(enum ETestcaseDataId dataId, unsigned edgeIdx) {
     default:
         abort();
     }
-
 }
